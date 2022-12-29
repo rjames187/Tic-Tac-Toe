327d0ee1a69c91cb286126fcffb146b29ef2b45c
rjames187 <rory.james2021@gmail.com>
2022-12-29 16:45:04 -0600

    write gameboard module

---
+++ script.js
@@ -0,0 +1,41 @@
+const gameBoard = (() => {
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+    const checkRow = (indices) => {
+        left = gameBoard[indices[0]];
+        right = gameBoard[indices[1]];
+        middle = gameboard[indices[2]];
+        if (left === "O" || left == "X") {
+            if (left === middle && left === right) {
+                return true
+            }
+        }
+        return false
+    };
+    const checkForWins = () => {
+        const COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
+        for (combo of COMBOS) {
+            if (checkRow(combo)) {
+                break;
+            }
+        }
+    };
+    const placeMarker = (marker, spot) => {
+        if (marker !== "O" && marker !== "X") {
+            return "error";
+        }
+        if (gameBoard[spot] !== "") {
+            return "error";
+        }
+        gameBoard[spot] = marker;
+        return "okay";
+    }
+    const getGameBoard = () => {
+        return gameBoard;
+    }
+
+    return {
+        checkForWins,
+        placeMarker,
+        getGameBoard
+    }
+})();
