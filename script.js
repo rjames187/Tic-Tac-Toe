778500117e7305fd8ffbf36fc18659fbf6cfd82d
rjames187 <rory.james2021@gmail.com>
2022-12-29 17:08:48 -0600

    add basic structure and style to prepare for displayController

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ const gameBoard = (() => {
         const COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
         for (combo of COMBOS) {
             if (checkRow(combo)) {
-                break;
+                return gameBoard[combo[0]];
             }
+        }
+        if (!gameBoard.includes("")){
+            return "tie";
         }
     };
     const placeMarker = (marker, spot) => {
